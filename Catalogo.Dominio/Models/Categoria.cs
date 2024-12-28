4835dc9b8e79e20e7152a0862b3c6e42ee849fb2
Yeisson Villamil <yeisson.villamil@globalmvm.com>
2024-12-27 19:38:37 -0500

    se definieron los modelos necesarios

---
+++ Catalogo.Dominio/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalogo.Dominio.Models
+{
+    //Representa una categoria de productos
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        
+        public List<Producto> Productos { get; set; } = new List<Producto>();
+    }
+}
---
+++ Catalogo.Dominio/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalogo.Dominio.Models
+{
+    //Representa un producto en el catalogo de la tienda
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Stock { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public bool Activo { get; set; } //Indica si el producto esta activo o no
+    }
+}
